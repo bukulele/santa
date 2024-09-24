17fe9220328fac2dafb638efde0f49feda4a09e3
Nikita Sazonov <kit.sazonov@gmail.com>
2024-09-24 11:21:37 -0300

    fix settings block component

--- app/components/SettingsBlock.js
+++ app/components/SettingsBlock.js
@@ -1,3 +1,7 @@
+"use client";
+
+import React from "react";
+
 function SettingsBlock({ children }) {
   return (
     <div className="bg-white rounded p-4 shadow text-black flex flex-col gap-2 w-96 max-w-full">
